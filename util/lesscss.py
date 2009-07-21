fe9362da7920820ca678abf257974c1938c90406
Mark Pilgrim <mark@diveintomark.org>
2009-07-21 00:09:24 -0400

    build fiddling

--- util/lesscss.py
+++ util/lesscss.py
@@ -26,7 +26,8 @@ original_css = raw_data.split('<style>', 1)[1].split('</style>', 1)[0]
 new_css = ''
 for rule in original_css.split('}')[:-1]:
     selectors, properties = rule.split('{', 1)
-    selectors = ','.join([s for s in selectors.split(',') if keep(s) or pqd(s.split(':', 1)[0])])
+    if selectors.count('@') == 0:
+        selectors = ','.join([s for s in selectors.split(',') if keep(s) or pqd(s.split(':', 1)[0])])
     if selectors:
         new_css += '%s{%s}' % (selectors, properties)
 open(filename, 'wb').write(raw_data.replace(original_css, new_css))
