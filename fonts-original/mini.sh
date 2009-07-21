80e1b984110324fe1c7de95b0760270f35822854
Mark Pilgrim <mark@diveintomark.org>
2009-07-20 23:47:04 -0400

    font fiddling

---
+++ fonts-original/mini.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cd ../../font-optimizer/ && perl subset.pl --charsfile=../diveintohtml5/fonts-original/chars ../diveintohtml5/fonts-original/Goudy_Bookletter_1911.ttf ../diveintohtml5/f/goudy-bookletter-min.ttf && cd -
