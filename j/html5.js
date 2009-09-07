8ed21214bcdfffcf835c63d08dedf0c9658920eb
Mark Pilgrim <mark@diveintomark.org>
2009-09-07 00:57:54 -0400

    finished #canvas and #canvas-text sections of detection chapter

--- j/html5.js
+++ j/html5.js
@@ -1,1 +1,1 @@
-(function(){var e="abbr,article,aside,audio,bb,canvas,datagrid,datalist,details,dialog,figure,footer,header,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while(i--){document.createElement(e[i])}})()
+(function(){var e="abbr,article,aside,audio,canvas,datalist,details,dialog,figure,footer,header,mark,menu,meter,nav,output,progress,section,time,video".split(','),i=e.length;while(i--){document.createElement(e[i])}})()
