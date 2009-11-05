b042eb7d7cede7ca6bb13a87c73e486fc3b8f558
Mark Pilgrim <mark@diveintomark.org>
2009-11-05 02:22:57 +0100

    first cut of geolocation chapter

--- fonts-original/mini.sh
+++ fonts-original/mini.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd ../../font-optimizer/ && perl subset.pl --charsfile=../diveintohtml5/fonts-original/chars ../diveintohtml5/fonts-original/Goudy_Bookletter_1911.ttf ../diveintohtml5/f/goudy-bookletter-min.ttf && cd -
+cd font-optimizer/ && perl subset.pl --charsfile=../chars ../LinLibertine_Re-4.4.1.ttf ../../f/lin-libertine-min.ttf lin-libertine-min.eot
