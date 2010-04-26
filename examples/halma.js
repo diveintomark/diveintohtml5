f036089193f3e80d9e6993944636b5ca6645f85e
Mark Pilgrim <mark@diveintomark.org>
2010-04-25 22:52:02 -0400

    simple board game to be used for canvas chapter and adding features in subsequent chapters

---
+++ examples/halma.js
@@ -0,0 +1,181 @@
+var kBoardWidth = 9;
+var kBoardHeight= 9;
+var kPieceWidth = 50;
+var kPieceHeight= 50;
+var kPixelWidth = 1 + (kBoardWidth * kPieceWidth);
+var kPixelHeight= 1 + (kBoardHeight * kPieceHeight);
+var gCanvasElement;
+var gDrawingContext;
+var gPieces;
+var gNumPieces;
+var gSelectedPieceIndex;
+var gSelectedPieceHasMoved;
+var gMoveCount;
+
+function Cell(row, column) {
+    this.row = row;
+    this.column = column;
+}
+
+function getCursorPosition(e) {
+    /* returns Cell with .row and .column properties */
+    var x;
+    var y;
+    if (e.pageX || e.pageY) {
+	x = e.pageX;
+	y = e.pageY;
+    }
+    else {
+	x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+	y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+    x -= gCanvasElement.offsetLeft;
+    y -= gCanvasElement.offsetTop;
+    x = Math.min(x, kBoardWidth * kPieceWidth);
+    y = Math.min(y, kBoardHeight * kPieceHeight);
+    var cell = new Cell(Math.floor(y/kPieceWidth), Math.floor(x/kPieceHeight));
+    return cell;
+}
+
+function onclick(e) {
+    var cell = getCursorPosition(e);
+    for (var i = 0; i < gNumPieces; i++) {
+	if ((gPieces[i].row == cell.row) && 
+	    (gPieces[i].column == cell.column)) {
+	    clickOnPiece(i);
+	    return;
+	}
+    }
+    clickOnEmptyCell(cell);
+}
+
+function clickOnEmptyCell(cell) {
+    if (gSelectedPieceIndex == -1) { return; }
+    var rowDiff = Math.abs(cell.row - gPieces[gSelectedPieceIndex].row);
+    var columnDiff = Math.abs(cell.column - gPieces[gSelectedPieceIndex].column);
+    if ((rowDiff <= 1) &&
+	(columnDiff <= 1)) {
+	/* we already know that this click was on an empty square,
+	   so that must mean this was a valid single-square move */
+	gPieces[gSelectedPieceIndex].row = cell.row;
+	gPieces[gSelectedPieceIndex].column = cell.column;
+	gMoveCount += 1;
+	gSelectedPieceIndex = -1;
+	gSelectedPieceHasMoved = false;
+	drawBoard();
+	return;
+    }
+    if ((((rowDiff == 2) && (columnDiff == 0)) ||
+	 ((rowDiff == 0) && (columnDiff == 2)) ||
+	 ((rowDiff == 2) && (columnDiff == 2))) && 
+	isThereAPieceBetween(gPieces[gSelectedPieceIndex], cell)) {
+	/* this was a valid jump */
+	if (!gSelectedPieceHasMoved) {
+	    gMoveCount += 1;
+	}
+	gSelectedPieceHasMoved = true;
+	gPieces[gSelectedPieceIndex].row = cell.row;
+	gPieces[gSelectedPieceIndex].column = cell.column;
+	drawBoard();
+	return;
+    }
+    gSelectedPieceIndex = -1;
+    gSelectedPieceHasMoved = false;
+    drawBoard();
+}
+
+function clickOnPiece(pieceIndex) {
+    if (gSelectedPieceIndex == pieceIndex) { return; }
+    gSelectedPieceIndex = pieceIndex;
+    gSelectedPieceHasMoved = false;
+    drawBoard();
+}
+
+function isThereAPieceBetween(cell1, cell2) {
+    /* note: assumes cell1 and cell2 are 2 squares away
+       either vertically, horizontally, or diagonally */
+    var rowBetween = (cell1.row + cell2.row) / 2;
+    var columnBetween = (cell1.column + cell2.column) / 2;
+    for (var i = 0; i < gNumPieces; i++) {
+	if ((gPieces[i].row == rowBetween) &&
+	    (gPieces[i].column == columnBetween)) {
+	    return true;
+	}
+    }
+    return false;
+}
+
+function drawBoard() {
+    gDrawingContext.clearRect(0, 0, kPixelWidth, kPixelHeight);
+
+    gDrawingContext.beginPath();
+    
+    /* vertical lines */
+    for (var x = 0; x <= kPixelWidth; x += kPieceWidth) {
+	gDrawingContext.moveTo(0.5 + x, 0);
+	gDrawingContext.lineTo(0.5 + x, kPixelHeight);
+    }
+    
+    /* horizontal lines */
+    for (var y = 0; y <= kPixelHeight; y += kPieceHeight) {
+	gDrawingContext.moveTo(0, 0.5 + y);
+	gDrawingContext.lineTo(kPixelWidth, 0.5 +  y);
+    }
+    
+    /* draw it! */
+    gDrawingContext.strokeStyle = "#ccc";
+    gDrawingContext.stroke();
+    
+    for (var i = 0; i < 9; i++) {
+	drawPiece(gPieces[i], i == gSelectedPieceIndex);
+    }
+
+    document.getElementById("movecount").innerHTML = gMoveCount;
+}
+
+function drawPiece(p, selected) {
+    var column = p.column;
+    var row = p.row;
+    var x = (column * kPieceWidth) + (kPieceWidth/2);
+    var y = (row * kPieceHeight) + (kPieceHeight/2);
+    var radius = (kPieceWidth/2) - (kPieceWidth/10);
+    gDrawingContext.beginPath();
+    gDrawingContext.arc(x, y, radius, 0, Math.PI*2);
+    gDrawingContext.closePath();
+    gDrawingContext.strokeStyle = "#000";
+    gDrawingContext.stroke();
+    if (selected) {
+	gDrawingContext.fillStyle = "#000";
+	gDrawingContext.fill();
+    }
+}
+
+function newGame() {
+    gPieces = [new Cell(6, 0),
+	       new Cell(7, 0),
+	       new Cell(8, 0),
+	       new Cell(6, 1),
+	       new Cell(7, 1),
+	       new Cell(8, 1),
+	       new Cell(6, 2),
+	       new Cell(7, 2),
+	       new Cell(8, 2)];
+    gNumPieces = gPieces.length;
+    gSelectedPieceIndex = -1;
+    gSelectedPieceHasMoved = false;
+    gMoveCount = 0;
+    drawBoard();
+}
+
+function init() {
+    gCanvasElement = document.createElement("canvas");
+    gCanvasElement.id = "c";
+    gCanvasElement.width = kPixelWidth;
+    gCanvasElement.height = kPixelHeight;
+    document.body.appendChild(gCanvasElement);
+    gCanvasElement.addEventListener("click", onclick, false);
+    gDrawingContext = gCanvasElement.getContext("2d");
+    newGame();
+}
+
+window.onload = init;
